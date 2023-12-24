4d15ce3671ca8afbff57ee570fe8a329dfb218a6
Juan Bratti <juanbratti02@gmail.com>
2023-12-24 20:40:09 -0300

    cropping files

---
+++ CV_basics/src/cropping.py
@@ -0,0 +1,14 @@
+import cv2
+import os
+
+img = cv2.imread(os.path.join('..', 'data', 'hydrangea.jpeg'))
+
+print(img.shape)
+
+# (height interval, width interval)
+# center is top left.
+cropped_img = img[90:357, 324:584]
+cv2.imshow('Image Visualizer', img)
+cv2.imshow('Image Cropped Visualizer', cropped_img)
+
+cv2.waitKey(0)
